da40ded3293896996369adb26691d2928c65c0b2
sjtcumt <1192472945@qq.com>
2018-03-16 21:37:11 +0800

    remove old

--- proj01/src/base/entry.go
+++ proj01/src/base/entry.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	Twlog "twlog"
 )
 
 const (
@@ -19,17 +18,16 @@ func NewEmptyEntry(Kind string) *EntryStu {
 }
 
 type EntryStu struct {
-	Id         string         `json:"msgid"`
-	PathId     string         `json:"pathid"`
-	Kind       string         `json:"framekind"`
-	ExecStatus int            `json:"execstatus"`
-	ErrCode    int            `json:"errcode"`
-	Method     string         `json:"method"`
-	Url        string         `json:"url"`
-	StatusCode int            `json:"status"`
-	Header     http.Header    `json:"headers"`
-	Body       string         `json:"body"`
-	PrnLog     Twlog.TwLogStu `json:"-"`
+	Id         string      `json:"msgid"`
+	PathId     string      `json:"pathid"`
+	Kind       string      `json:"framekind"`
+	ExecStatus int         `json:"execstatus"`
+	ErrCode    int         `json:"errcode"`
+	Method     string      `json:"method"`
+	Url        string      `json:"url"`
+	StatusCode int         `json:"status"`
+	Header     http.Header `json:"headers"`
+	Body       string      `json:"body"`
 }
 
 func (r *EntryStu) Init(req *http.Request) (ret int) {
@@ -39,53 +37,17 @@ func (r *EntryStu) Init(req *http.Request) (ret int) {
 	r.SetMethod(Mothod)
 
 	Url := fmt.Sprintf("%v", req.URL)
-	if len(Url) <= 1 {
-		//		PrnLog.Error("EntryStu init req url is empty")
-		return -1
-	}
 	r.SetUrl(Url)
 
 	r.SetHeader(req.Header)
 
 	Body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		//		PrnLog.Error("EntryStu init read req body error")
 		return -1
 	}
 	r.SetBody(string(Body))
 
 	return
-}
-
-func (r *EntryStu) SetErrCode(ErrCode int) {
-	r.ErrCode = ErrCode
-}
-func (r *EntryStu) GetErrCode() int {
-	return r.ErrCode
-}
-func (r *EntryStu) SetExecStatus(ExecStatus int) {
-	r.ExecStatus = ExecStatus
-}
-func (r *EntryStu) GetExecStatus() int {
-	return r.ExecStatus
-}
-func (r *EntryStu) SetMethod(Method string) {
-	r.Method = Method
-}
-func (r *EntryStu) GetMethod() (Method string) {
-	return r.Method
-}
-func (r *EntryStu) SetHeader(Header http.Header) {
-	r.Header = Header
-}
-func (r *EntryStu) GetHeader() http.Header {
-	return r.Header
-}
-func (r *EntryStu) SetBody(Body string) {
-	r.Body = Body
-}
-func (r *EntryStu) GetBody() (Body string) {
-	return r.Body
 }
 func (r *EntryStu) SetId(Id string) {
 	r.Id = Id
@@ -99,11 +61,29 @@ func (r *EntryStu) SetKind(Kind string) {
 func (r *EntryStu) GetKind() (Kind string) {
 	return r.Kind
 }
+func (r *EntryStu) SetMethod(Method string) {
+	r.Method = Method
+}
+func (r *EntryStu) GetMethod() (Method string) {
+	return r.Method
+}
 func (r *EntryStu) SetUrl(Url string) {
 	r.Url = Url
 }
 func (r *EntryStu) GetUrl() (Url string) {
 	return r.Url
+}
+func (r *EntryStu) SetHeader(Header http.Header) {
+	r.Header = Header
+}
+func (r *EntryStu) GetHeader() http.Header {
+	return r.Header
+}
+func (r *EntryStu) SetBody(Body string) {
+	r.Body = Body
+}
+func (r *EntryStu) GetBody() (Body string) {
+	return r.Body
 }
 func (r *EntryStu) ToJson() string {
 	Data, _ := json.Marshal(r)
--- proj01/src/sysinfo/clients.go
+++ proj01/src/sysinfo/clients.go
@@ -1,12 +1,9 @@
 package sysinfo
 
 import (
-	"bytes"
-	"fmt"
-	"text/tabwriter"
-
 	. "base"
 	client "client"
+	tool "tool"
 )
 
 const (
@@ -23,52 +20,47 @@ func init() {
 
 type CliListStu struct {
 	*EntryStu
-	Format   string
-	Tw       *tabwriter.Writer
-	Buf      *bytes.Buffer
 	EntryRst *EntryStu
+	*tool.TwLogStu
 }
 
 func (r *CliListStu) InitPrn() (ret int) {
-	r.Format = "%v\t%v\t\n"
-	r.Buf = new(bytes.Buffer)
-	r.Tw = new(tabwriter.Writer).Init(r.Buf, 0, 8, 2, ' ', 0)
-	r.PrnLog("clients list", " detail ")
+	r.TwLogStu = tool.NewTwLogStu("%v\t%v\t\n")
+
 	return
 }
-func (r *CliListStu) PrnLog(arg1, arg2 string) {
-	fmt.Fprintf(r.Tw, r.Format, arg1, arg2)
-}
+
 func (r *CliListStu) ScanClients() (ret int) {
 	Clients := client.GetClientsStu()
-	for _, v := range Clients.Clients {
-		r.PrnLog("", v.GetId())
+	for k, v := range Clients.Clients {
+		r.Print(k, v.GetId())
 	}
 	return
 }
 func (r *CliListStu) CreateRst() (ret int) {
 	r.EntryRst = NewEmptyEntry("")
-	r.Tw.Flush()
-	r.EntryRst.SetBody(r.Buf.String())
+	r.EntryRst.SetBody(r.Flush())
 	return
 }
 func (r *CliListStu) Execute() (EntryOut *EntryStu, ret int) {
 	defer func() {
 		EntryOut = r.EntryRst
 	}()
+
 	ret = r.InitPrn()
+	r.Print("clilist", "--list--")
 	if ret != 0 {
-		r.PrnLog("error", "r.InitPrn")
+		r.Print("error", "r.InitPrn")
 		return
 	}
 	ret = r.ScanClients()
 	if ret != 0 {
-		r.PrnLog("error", "r.ScanClients")
+		r.Print("error", "r.ScanClients")
 		return
 	}
 	ret = r.CreateRst()
 	if ret != 0 {
-		r.PrnLog("error", "r.CreateRst")
+		r.Print("error", "r.CreateRst")
 		return
 	}
 	return
